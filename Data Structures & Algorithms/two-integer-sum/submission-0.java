a4318ddae0347c2862318994c0dc4a1d471c2671
Alex Khalin <31737047+alexkhalin@users.noreply.github.com>
2026-09-17 11:28:58 +0200

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        final int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>(2000);
+        for (int i = 0; i < len; i++) {
+            int num = nums[i];
+            Integer second = map.get(target - num);
+            if (second != null) return new int[]{second, i};
+            map.put(num, i);
+        }
+
+        throw new RuntimeException("Pair not found");
+    }
+}
